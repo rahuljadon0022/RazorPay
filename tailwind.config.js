96fd8e790869da4706a42f89c9dcabfb919a1375
rahuljadon0022 <167974255+rahuljadon0022@users.noreply.github.com>
2025-01-20 14:54:43 +0530

    Nav Bar Code Of Razor Pay website.

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily:{
+        mullish: ["Mulish","sans-serif"],
+      },
+    
+    },
+  },
+  plugins: [],
+}
+
